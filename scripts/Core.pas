0e62053712ee72653bb81b1c0fa752570d9731f8
ArchiGoodwin <agnw36@gmail.com>
2017-01-21 01:38:41 +0300

    added core class
    it should be a start point of process

--- scripts/Core.pas
+++ scripts/Core.pas
@@ -1,3 +1,85 @@
-// TODO:
+unit Farming;
 
-//TODO: do all and earn adena! =)
+interface
+
+uses SysUtils, Classes, Moving, UserConfig;
+
+var
+	m_userState: TUserConfig; 
+	
+// Init function. Can be replaced with True Class constructor
+function InitializeLocalVariables(): integer;	
+// TODO: fix it - it doesn't write log anywhere. Also make sense to write special function to send IM in ICQ
+// Print message
+function AddMsgToLog(p_strMsg: string): integer;
+
+
+implementation
+
+function InitializeLocalVariables(): integer;
+begin
+	Result := 0;
+	
+	if (Engine.Status = lsOffline) then
+	begin
+		Result := -1;
+	end
+	else begin
+		Print('InitializeLocalVariables');
+		m_userState := UserConfig.LoadUserConfig(User.Name);
+		
+		// StartLogSession(m_userState);
+		
+		// TODO: load some special variables for current character
+		with m_userState do
+		begin
+			IsDead := (User.HP = 0);
+			// CurrentPath: string; it should be changed in Hunting Zone logic
+			//EquippedWeaponID : integer;
+		end;
+		
+	end;
+end;
+
+function AddMsgToLog(p_strMsg: string): integer;
+begin
+  Result := 0;
+  Print(p_strMsg);
+end;
+
+
+// ------------------------------------
+// Main body
+begin
+	Print('Start script.');
+	RndDelay(1500);
+
+	// Try to init vars
+	if (InitializeLocalVariables() = -1) then
+	begin
+		Print('Cannot initialize character.');
+		Exit();
+	end;
+	
+	if (GoHomeIfDead() > -1) then 
+	begin
+		//TODO: check if user in town then go to gk
+		// MoveToGK();
+
+		// TODO: need to know how much hunting zones the character has already visited.
+		// Need to save into special log the list of these zones.
+		// Need to select the next hunting zone where the char is in Town.
+			
+		// Check if user in the hunting zone then start farming
+		// Init successfully, start boting process
+		// List of checks in threads  
+		// Script.NewThread(@CheckCharIsLocked()); 
+		// Script.NewThread(@CheckCharIsDebuffed()); 	
+		FarmMobsInHuntingZone();
+	end;
+	//until Engine.Status = lsOffline;
+	
+end.
+
+
+ 
---
+++ scripts/PathKlerik.pas
@@ -0,0 +1,302 @@
+// TODO: for 20 lvl char
+  
+  // Engine.DlgSel(1);
+  Engine.MoveTo(-80139, 150860, -3067);
+  Engine.MoveTo(-80529, 150780, -3067);
+  Engine.MoveTo(-80767, 150838, -3061);
+  Engine.MoveTo(-80874, 150481, -3070);
+  Engine.MoveTo(-80733, 150229, -3070);
+  Engine.MoveTo(-80728, 149988, -3070);
+  Engine.MoveTo(-80734, 149826, -3070);
+  Engine.SetTarget(30320);   // Ричин
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+  Engine.MoveTo(-14455, 123986, -3120);
+  Engine.MoveTo(-14822, 123708, -3112);
+  Engine.MoveTo(-14742, 123768, -3120);
+  Engine.MoveTo(-14831, 123453, -3141);
+  Engine.MoveTo(-14991, 123415, -3134);
+  Engine.MoveTo(-15127, 123377, -3134);
+  Engine.MoveTo(-15139, 123366, -3104);
+  Engine.SetTarget(30253);   // Симплон
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+  Engine.MoveTo(-14988, 123440, -3134);
+  Engine.MoveTo(-14533, 123428, -3149);
+  Engine.MoveTo(-14159, 122605, -3143);
+  Engine.MoveTo(-14070, 122082, -3015);
+  Engine.MoveTo(-13896, 121775, -2994);
+  Engine.MoveTo(-13669, 121665, -2994);
+  Engine.MoveTo(-13372, 121801, -2994);
+  Engine.MoveTo(-13282, 121845, -2994);
+  Engine.SetTarget(30030);   // Вивиан
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.CancelTarget;
+  Engine.MoveTo(-13528, 121751, -2994);
+  Engine.MoveTo(-13878, 121703, -2994);
+  Engine.MoveTo(-14119, 121718, -3015);
+  Engine.MoveTo(-14173, 122067, -3015);
+  Engine.MoveTo(-14236, 122727, -3139);
+  Engine.MoveTo(-14699, 123549, -3147);
+  Engine.MoveTo(-15091, 123875, -3154);
+  Engine.MoveTo(-16205, 124118, -3158);
+  Engine.MoveTo(-16573, 124063, -3157);
+  Engine.MoveTo(-16615, 124044, -3158);
+  Engine.SetTarget(30333);   // Праг
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+
+
+
+  Engine.MoveTo(-14988, 123440, -3134);
+  Engine.MoveTo(-14533, 123428, -3149);
+  Engine.MoveTo(-14159, 122605, -3143);
+  Engine.MoveTo(-14070, 122082, -3015);
+  Engine.MoveTo(-13896, 121775, -2994);
+  Engine.MoveTo(-13669, 121665, -2994);
+  Engine.MoveTo(-13372, 121801, -2994);
+  Engine.MoveTo(-13282, 121845, -2994);
+  Engine.SetTarget(30030);   // Вивиан
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.CancelTarget;
+  Engine.MoveTo(-13528, 121751, -2994);
+  Engine.MoveTo(-13878, 121703, -2994);
+  Engine.MoveTo(-14119, 121718, -3015);
+  Engine.MoveTo(-14173, 122067, -3015);
+  Engine.MoveTo(-14236, 122727, -3139);
+  Engine.MoveTo(-14699, 123549, -3147);
+  Engine.MoveTo(-15091, 123875, -3154);
+  Engine.MoveTo(-16205, 124118, -3158);
+  Engine.MoveTo(-16573, 124063, -3157);
+  Engine.MoveTo(-16615, 124044, -3158);
+  Engine.SetTarget(30333);   // Праг
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.MoveTo(-16408, 124097, -3157);
+  Engine.MoveTo(-15962, 124134, -3161);
+  Engine.MoveTo(-14970, 123805, -3152);
+  Engine.MoveTo(-14646, 123729, -3147);
+  Engine.MoveTo(-14568, 123863, -3152);
+  Engine.MoveTo(-14545, 124004, -3151);
+  Engine.SetTarget(30256);   // Белла
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243419504_4_57_1209035019_3");
+  Engine.MoveTo(-80826, 149775, -3040);
+  Engine.SetTarget(30320);   // Ричин
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243425456_6_57_1209034968_3");
+
+  Engine.MoveTo(-69894, 115209, -3563);
+  Engine.MoveTo(-68240, 115048, -3571);
+  Engine.MoveTo(-68331, 115112, -3536);
+  Engine.MoveTo(-68200, 115032, -3566);
+  Engine.MoveTo(-68249, 114970, -3566);
+  Engine.MoveTo(-68307, 115097, -3536);
+  Engine.MoveTo(-67665, 114349, -3538);
+  Engine.MoveTo(-66322, 113532, -3559);
+  Engine.MoveTo(-64868, 112688, -3654);
+  Engine.MoveTo(-62304, 111004, -3628);
+  Engine.MoveTo(-59076, 108751, -3281);
+  Engine.MoveTo(-61351, 110779, -3571);
+  Engine.MoveTo(-60182, 110740, -3550);
+  Engine.MoveTo(-58862, 110115, -3499);
+  Engine.MoveTo(-58309, 110240, -3420);
+  Engine.MoveTo(-57963, 110412, -3400);
+// TODO: here need to kill zombies 
+  //Engine.SetTarget(20026);   // Зомби Руин
+  //Engine.SetTarget(20029);   // Опытный Зомби Руин
+// TODO: continue after quest item is enough
+// TODO: use SOE
+  Engine.CancelTarget;
+  Engine.MoveTo(-14491, 121915, -3048);
+  Engine.MoveTo(-14499, 122534, -3152);
+  Engine.MoveTo(-14679, 123429, -3147);
+  Engine.MoveTo(-14803, 123796, -3151);
+  Engine.MoveTo(-16295, 124124, -3158);
+  Engine.MoveTo(-16520, 124109, -3158);
+  Engine.MoveTo(-16599, 124050, -3158);
+  Engine.SetTarget(30333);   // Праг
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.MoveTo(-15358, 123982, -3159);
+  Engine.MoveTo(-14696, 123962, -3157);
+  Engine.MoveTo(-14576, 124037, -3151);
+  Engine.SetTarget(30256);   // Белла
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243419504_4_57_1209035019_3");
+  Engine.MoveTo(-80787, 150499, -3070);
+  Engine.MoveTo(-80739, 150583, -3070);
+  Engine.MoveTo(-80789, 150510, -3070);
+  Engine.MoveTo(-80800, 150819, -3061);
+  Engine.MoveTo(-79931, 150856, -3067);
+  Engine.MoveTo(-79354, 150729, -3067);
+  Engine.MoveTo(-79268, 150695, -3067);
+  Engine.SetTarget(30022);   // Зигонт
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.MoveTo(-79795, 150761, -3067);
+  Engine.MoveTo(-80425, 150830, -3067);
+  Engine.MoveTo(-80814, 150791, -3061);
+  Engine.MoveTo(-81031, 150794, -3070);
+  Engine.MoveTo(-80821, 150202, -3070);
+  Engine.MoveTo(-80817, 149949, -3070);
+  Engine.MoveTo(-80766, 149832, -3070);
+  Engine.SetTarget(30320);   // Ричин
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243425456_0_57_1209034968_3");
+  Engine.MoveTo(-84162, 244566, -3755);
+  Engine.SetTarget(30006);   // Рокси
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243872352_1_57_1209034423_3");
+  Engine.MoveTo(-100030, 237026, -3585);
+  Engine.MoveTo(-101235, 236686, -3479);
+  Engine.MoveTo(-101656, 236530, -3417);
+  Engine.MoveTo(-102400, 236320, -3434);
+  Engine.MoveTo(-104234, 236547, -3726);
+  Engine.MoveTo(-103138, 236345, -3564);
+  Engine.MoveTo(-103340, 236442, -3568);
+  Engine.MoveTo(-103336, 236441, -3567);
+  Engine.MoveTo(-103331, 236440, -3565);
+  Engine.MoveTo(-103325, 236438, -3564);
+  Engine.MoveTo(-103320, 236437, -3563);
+  Engine.MoveTo(-103314, 236436, -3561);
+  Engine.MoveTo(-103307, 236434, -3560);
+  Engine.MoveTo(-104510, 236004, -3674);
+  Engine.MoveTo(-105194, 236046, -3749);
+  Engine.MoveTo(-106739, 234402, -3677);
+  Engine.MoveTo(-106642, 233955, -3645);
+  Engine.MoveTo(-106515, 233453, -3652);
+  Engine.MoveTo(-106489, 233409, -3655);
+  Engine.SetTarget(30408);   // Лионель
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+// TODO: SOE 
+
+  Engine.MoveTo(-83608, 244767, -3755);
+  Engine.MoveTo(-83080, 244594, -3755);
+  Engine.MoveTo(-82395, 244887, -3750);
+  Engine.MoveTo(-82017, 245876, -3728);
+  Engine.MoveTo(-82222, 246721, -3680);
+  Engine.MoveTo(-81974, 248505, -3718);
+  Engine.MoveTo(-82936, 250048, -3690);
+  Engine.MoveTo(-84060, 251342, -3686);
+  Engine.MoveTo(-85328, 251388, -3613);
+  Engine.MoveTo(-87043, 250653, -3640);
+  Engine.MoveTo(-88089, 249912, -3619);
+  Engine.MoveTo(-88371, 249129, -3601);
+  Engine.MoveTo(-88894, 248720, -3601);
+  Engine.MoveTo(-89593, 248554, -3601);
+  Engine.MoveTo(-90148, 248340, -3598);
+  Engine.MoveTo(-90762, 248061, -3597);
+  Engine.MoveTo(-90960, 248025, -3591);
+  Engine.SetTarget(30017);   // Галлинт
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+Engine.BypassToServer("quest_choice?choice=4&option=1");
+  Engine.MoveTo(-90877, 248067, -3597);
+  Engine.MoveTo(-90603, 248290, -3597);
+  Engine.MoveTo(-89961, 248666, -3601);
+  Engine.MoveTo(-88829, 248835, -3601);
+  Engine.MoveTo(-88240, 249076, -3601);
+  Engine.MoveTo(-88121, 249763, -3616);
+  Engine.MoveTo(-86291, 250863, -3619);
+  Engine.MoveTo(-85294, 251091, -3601);
+  Engine.MoveTo(-84123, 251196, -3671);
+  Engine.MoveTo(-83092, 250285, -3689);
+  Engine.MoveTo(-82737, 249078, -3645);
+  Engine.MoveTo(-82448, 247568, -3632);
+  Engine.MoveTo(-82471, 245249, -3735);
+  Engine.MoveTo(-82634, 244636, -3755);
+  Engine.MoveTo(-83193, 244599, -3755);
+  Engine.MoveTo(-83714, 244724, -3755);
+  Engine.MoveTo(-84054, 244629, -3755);
+  Engine.SetTarget(30006);   // Рокси
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("teleport_request");
+Engine.BypassToServer("teleport_4243872352_1_57_1209034423_3");
+  Engine.MoveTo(-100030, 237026, -3585);
+  Engine.MoveTo(-101235, 236686, -3479);
+  Engine.MoveTo(-101656, 236530, -3417);
+  Engine.MoveTo(-102400, 236320, -3434);
+  Engine.MoveTo(-104234, 236547, -3726);
+  Engine.MoveTo(-103138, 236345, -3564);
+  Engine.MoveTo(-103340, 236442, -3568);
+  Engine.MoveTo(-103336, 236441, -3567);
+  Engine.MoveTo(-103331, 236440, -3565);
+  Engine.MoveTo(-103325, 236438, -3564);
+  Engine.MoveTo(-103320, 236437, -3563);
+  Engine.MoveTo(-103314, 236436, -3561);
+  Engine.MoveTo(-103307, 236434, -3560);
+  Engine.MoveTo(-104510, 236004, -3674);
+  Engine.MoveTo(-105194, 236046, -3749);
+  Engine.MoveTo(-106739, 234402, -3677);
+  Engine.MoveTo(-106642, 233955, -3645);
+  Engine.MoveTo(-106515, 233453, -3652);
+  Engine.MoveTo(-106489, 233409, -3655);
+  Engine.SetTarget(30408);   // Лионель
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+
+//TODO: SOE - to talking island or to Gludin
+
+  Engine.MoveTo(-80803, 149892, -3070);
+  Engine.MoveTo(-80806, 150256, -3070);
+  Engine.MoveTo(-80802, 150827, -3061);
+  Engine.MoveTo(-80044, 150838, -3067);
+  Engine.MoveTo(-79199, 150839, -3067);
+  Engine.MoveTo(-79257, 150707, -3067);
+  Engine.SetTarget(30022);   // Зигонт
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("talk_select");
+  Engine.MoveTo(-79140, 150796, -3067);
+  Engine.MoveTo(-79012, 150830, -3058);
+  Engine.SetTarget(30037);   // Левиан
+  Engine.DlgOpen;
+  // тут записываем диалог
+  // Engine.DlgSel(1);
+Engine.BypassToServer("menu_select?ask=-2&reply=1");
+Engine.BypassToServer("class_change?class_name=15");
+// TODO: end.
+
+
